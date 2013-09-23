869eb67c622f8e66123c25c06543df648e4f2d43
Viswa Narahari <viswanatham@gmail.com>
2013-09-23 13:08:00 -0400

    Initial commit on 9/23

---
+++ KnightPyDev/src/FirstModule.py
@@ -0,0 +1,45 @@
+'''
+Created on Sep 12, 2013
+
+@author: g7-1310us
+
+
+'''
+# http://stackoverflow.com/questions/6667201/how-to-define-two-dimensional-array-in-python
+ 
+
+
+
+
+ 
+def nextHop(xy,r,c):
+    currentRow = xy // 8
+    goodRow    = currentRow + r 
+    newxy      = xy + (8 * r) + c 
+    newRow     = newxy // 8
+    if ( (newxy < 0 ) or ( newxy > 63 ) or ( newRow != goodRow )):
+        newxy = 20   
+    return newxy
+
+xy = 0
+nxy = nextHop(xy, -2,-1); print (nxy)
+nxy = nextHop(xy, -2,1) ; print (nxy)
+nxy = nextHop(xy, -1,-2) ; print (nxy)
+nxy = nextHop(xy, -1,2) ; print (nxy)
+nxy = nextHop(xy, 1, -2) ; print (nxy)
+nxy = nextHop(xy, 1, 2) ; print (nxy)
+nxy = nextHop(xy, 2,-1) ; print (nxy)
+nxy = nextHop(xy, 2,1) ; print (nxy)
+
+
+
+
+
+
+
+
+
+        
+    
+    
+
